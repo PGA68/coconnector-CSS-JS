5ff10495552102035b158be810150baf74ed71c7
GAP68 <agdars@gmail.com>
2015-05-09 22:34:47 +0300

    ENG

--- c1.js
+++ c1.js
@@ -1,34 +1,34 @@
-!function($, W, D, M, wc, u7d){
+!function($, W, D, M, wc, u7d){	// 
     'use strict';
     var
-        phjs = '/gap/js/',
-        phcss = '/gap/css/';
+        phjs = '/gap/js/',	// path for js
+        phcss = '/gap/css/'; // path for css
         
-    function loadDynamic (src,type){
-        var node=document.createElement(type?"link":"script");
+    function loadDynamic (src, type){ // function trivial lazy loading js/css
+        var node=document.createElement(type ? "link" : "script");
         node = document.getElementsByTagName("head")[0].appendChild(node);
         /*
-        *   если передаем второй параметр, то это таблица стилей
+        *   if exist second parameter, then on css.
         */
         
         if(type){
-            node.setAttribute("rel","stylesheet");
-            node.setAttribute("media","all");
+            node.setAttribute("rel", "stylesheet");
+            node.setAttribute("media", "all");
         }
-        node.setAttribute("type","text/"+(type?"css":"javascript"));
-        node.setAttribute(type?"href":"src",src);
+        node.setAttribute("type","text/" + (type ? "css" : "javascript"));
+        node.setAttribute(type ? "href" : "src", src);
     }
-        
+	
     $(W).on('load',function(){
         $('[class^=dynload]').each(
             function(){
                 var gr = /dynload\S+-(\S+)/.exec(this.className)[1].split('_');
                 /*
-                *   class = 'dynload-gap-jsfilename_cssfilename'
-                *   gr = ['jsfilename','cssfilename'];
+                *   class	= 'dynload-gap-jsfilename_cssfilename'
+                *   gr	= ['jsfilename','cssfilename'];
                 */
                 
-                gr.length > 1 && loadDynamic(phcss+gr[1]+'.css',!0);
+                gr.length > 1 && loadDynamic(phcss+gr[1]+'.css', !0);
                 loadDynamic(phjs+gr[0]+'.js');
             });
         });
