ecf0596d6d2df439c84ae1dfd5043da9aa9c4bb7
GAP68 <agdars@gmail.com>
2015-05-09 23:46:59 +0300

    all

--- c1.js
+++ c1.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-п»ї!function($, W, D, M, wc, u7d){	// 
+!function($, W, D, M, wc, u7d){	// 
     'use strict';
     var
         phjs = '/gap/js/',	// path for js
@@ -20,47 +19,17 @@
         node.setAttribute(type ? "href" : "src", src);
     }
 	
-=======
-!function($, W, D, M, wc, u7d){
-    'use strict';
-    var
-        phjs = '/gap/js/',
-        phcss = '/gap/css/';
-        
-    function loadDynamic (src,type){
-        var node=document.createElement(type?"link":"script");
-        node = document.getElementsByTagName("head")[0].appendChild(node);
-        /*
-        *   если передаем второй параметр, то это таблица стилей
-        */
-        
-        if(type){
-            node.setAttribute("rel","stylesheet");
-            node.setAttribute("media","all");
-        }
-        node.setAttribute("type","text/"+(type?"css":"javascript"));
-        node.setAttribute(type?"href":"src",src);
-    }
-        
->>>>>>> 4c5165cce342426aa84c73a1cb77d9bbfe290d58
     $(W).on('load',function(){
         $('[class^=dynload]').each(
             function(){
                 var gr = /dynload\S+-(\S+)/.exec(this.className)[1].split('_');
                 /*
-<<<<<<< HEAD
                 *   class	= 'dynload-gap-jsfilename_cssfilename'
                 *   gr	= ['jsfilename','cssfilename'];
                 */
                 
                 gr.length > 1 && loadDynamic(phcss+gr[1]+'.css', !0);
-=======
-                *   class = 'dynload-gap-jsfilename_cssfilename'
-                *   gr = ['jsfilename','cssfilename'];
-                */
-                
-                gr.length > 1 && loadDynamic(phcss+gr[1]+'.css',!0);
->>>>>>> 4c5165cce342426aa84c73a1cb77d9bbfe290d58
+
                 loadDynamic(phjs+gr[0]+'.js');
             });
         });
